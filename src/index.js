1b7ca81264c3a7320021bbe883e80fb664d5bc21
unknown <artem.sych1989@gmail.com>
2022-02-25 08:35:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) {
+    return false;
+  }
+
+  const map = new Map(bracketsConfig);
+  let arr = [];
+  for (let el of str) {
+    let lastItem = arr[arr.length - 1];
+    if (el === map.get(lastItem)) {
+      arr.pop();
+    } else {
+      arr.push(el);
+    }
+  }
+  return arr.length === 0;
 }
